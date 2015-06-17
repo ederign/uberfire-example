402b261a4c9544b9d7e8dc9eaad5ed35ff427a71
ederign <ignatowicz@gmail.com>
2015-06-17 18:17:52 -0300

    app layout

---
+++ app/app-showcase/app-webapp/src/main/webapp/WEB-INF/classes/org/uberfire/client/screens/TasksGroupView.java
@@ -0,0 +1,34 @@
+package org.uberfire.client.screens;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.Dependent;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import org.uberfire.client.annotations.WorkbenchPartTitle;
+import org.uberfire.client.annotations.WorkbenchScreen;
+
+@Dependent
+@WorkbenchScreen(identifier = "TasksGroupView")
+public class TasksGroupView extends Composite {
+
+    interface Binder
+            extends
+            UiBinder<Widget, TasksGroupView> {
+
+    }
+
+    private static Binder uiBinder = GWT.create( Binder.class );
+
+    @PostConstruct
+    public void init() {
+        initWidget( uiBinder.createAndBindUi( this ) );
+    }
+
+    @WorkbenchPartTitle
+    public String getTitle() {
+        return "Dashboard";
+    }
+}
